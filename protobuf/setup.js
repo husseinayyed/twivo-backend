0a33f57bc393f96d1c8abd1f4f59f8e974ea6328
husseinayyed <honor9x.hasson@gmail.com>
2026-05-10 12:20:04 +0300

    feat(protobuf): add Twi message definition and serialization functions

--- protobuf/setup.js
+++ protobuf/setup.js
@@ -2,17 +2,19 @@ import protobuf from "protobufjs";
 import path from "path";
 
 // 1. Path to your .proto file
-const PROTO_PATH = path.join(process.cwd(), "protobuf","twivo.user.proto");
-
+const PROTO_USER_PATH = path.join(process.cwd(), "protobuf","twivo.user.proto");
+const PROTO_TWI_PATH = path.join(process.cwd(), "protobuf","twivo.twi.proto");
 // 2. Load the root synchronously or once at startup
 // In a high-performance backend, we cache this 'root' object
-const root = await protobuf.load(PROTO_PATH);
+const root = await protobuf.load(PROTO_USER_PATH);
+const rootForTwi = await protobuf.load(PROTO_TWI_PATH);
 
 // 3. Lookup all message types defined in your schema
 // Use the full namespace: package twivo.v1.user;
 const User = root.lookupType("twivo.v1.user.User");
 const PublicUser = root.lookupType("twivo.v1.user.PublicUser");
 
+const TwiType = root.lookupType("twivo.v1.twi.Twi");
 /**
  * Serializes a user object into a Protobuf binary buffer
  */
@@ -51,4 +53,35 @@ export function verifyUserMessage(data, isPublic) {
     }
 }
 
+
+export default function protoSerializeTwi(data) {
+    // Select the correct pre-loaded Type
+
+    // Map fields
+
+    // Validation
+    const verifyError = TwiType.verify(data);
+    if (verifyError) {
+        throw new Error(`Protobuf validation failed: ${verifyError}`);
+    }
+
+    // Create and Encode
+    const message = TwiType.create(data);
+    return TwiType.encode(message).finish();
+}
+export function verifyTwiMessage(data, isPublic) {
+
+    try {
+        const message = TwiType.decode(data);
+        const object = TwiType.toObject(message);
+        
+        // If verify returns null, the message is valid
+    
+      return TwiType.verify(object) === null;
+  
+    } catch (e) {
+        // If decode fails or throws, it's invalid
+           return false;
+    }
+}
 // Export the types in case you need them for .decode() elsewhere
